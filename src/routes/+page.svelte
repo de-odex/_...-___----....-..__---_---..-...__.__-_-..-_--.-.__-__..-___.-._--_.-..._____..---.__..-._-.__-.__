14a4b54b31dec34fab139cc9da75c1c9bcfbc3b1
de-odex <de-odex@users.noreply.github.com>
2025-04-25 05:25:11 +0900

    basic restructuring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,29 @@
 <script>
 	import { browser } from '$app/environment';
 	import * as F from '$lib/forth/forth.js';
+
+	let todos = $state([]);
+	let temp = $state('');
+
+	async function add() {}
+	async function toggle(temp) {}
+	async function delete_() {}
+	async function clear() {}
 </script>
 
 <h1>TODO: title...</h1>
 
-<input
-	placeholder="Buy milk…"
-	<!-- bind:value={} -->
-	<!-- on:keydown={(e) => e.key === 'Enter' && add()} -->
-/>
-<!-- <button on:click={add}>Add</button> -->
+<input placeholder="Buy milk..." bind:value={temp} onkeydown={(e) => e.key === 'Enter' && add()} />
+<button onclick={add}>Create Task</button> -->
+<button onclick={clear}>Delete All Tasks</button>
 
 <ul>
-	<!-- {#each todos as t}
+	{#each todos as t}
 		<li>
-			<input type="checkbox" checked={t.done} on:change={() => toggle(t.idx)} />
+			<input type="checkbox" checked={t.done} onchange={() => toggle(t.idx)} />
 			<span class:done={t.done}>{t.text}</span>
 		</li>
-	{/each} -->
+	{/each}
 </ul>
 
 <style>
